0c7afa6d69e2ccf4d88d1b14e6a8c156840f8b2f
shivangi-aneja <shivangi.aneja@tum.de>
2021-05-20 00:41:05 +0200

    cleanup unused files

--- dataset_visualizer/db_cursor.py
+++ dataset_visualizer/db_cursor.py
@@ -5,16 +5,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 mydb = mysql.connector.connect(
-    host="zerus.vc.in.tum.de",
-    user="aneja",
-    password="ge03shivangi",
+    host="localhost",
+    user="admin",
+    password="admin",
     database='NEWS_ARTICLES'
 )
 my_cursor = mydb.cursor(dictionary=True)
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://aneja:ge03shivangi@zerus.vc.in.tum.de/NEWS_ARTICLES'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:admin@localhost/NEWS_ARTICLES'
 app.config['SECRET_KEY'] = 'mysecret'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
